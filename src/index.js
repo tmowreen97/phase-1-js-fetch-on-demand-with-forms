221726fe239b9cccc5eb36729c16116815210640
Tasreen Mowreen <tmowreen97@gmail.com>
2022-03-20 00:54:01 -0400

    commit tree 5c278f7d050a26ab6f2294751a63dd6777676ca7

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 const init = () => {
-  
+  const inputForm = document.querySelector('form');
+  inputForm.addEventListener('submit', (e)=> {
+    e.preventDefault();
+    console.log(e);
+  })
 }
 
 document.addEventListener('DOMContentLoaded', init);
